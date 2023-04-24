ec42f0a1541067ad29e77a7b11ce9a1f13c57a4d
ChoiBongGeun <dhy00085@naver.com>
2023-04-24 15:50:29 +0900

    스케쥴 리스트 테이블에 잘 표시 되는지 확인

--- demo_front/src/pages/Scheduler/SchedulerPage.tsx
+++ demo_front/src/pages/Scheduler/SchedulerPage.tsx
@@ -1,23 +1,94 @@
 import { useCallback, useEffect, useState } from "react";
 import useStores from "../../hooks/useStore";
+import { IScheduler, ISchedulerSearchParams } from "../../interface/scheduler/IScheduler";
+import { useReactTable, ColumnDef, ColumnResizeMode, getCoreRowModel, getPaginationRowModel, getSortedRowModel, SortingState } from "@tanstack/react-table";
+import Table from "../../components/elements/Table";
 
 const SchedulerPage =() => {
     const [open, setOpen] = useState(false);
     const { schedulerStore } = useStores();
     const [loading, setLoading] = useState(false);
+    const columnResizeMode: ColumnResizeMode = 'onChange';
+    const [sorting, setSorting] = useState<SortingState>([]);
+    const [columnVisibility, setColumnVisibility] = useState({});
+
 
     useEffect(() => {
-        schedulerStore.search();
-        console.log(schedulerStore)
+        handleSearch({});
     }, [schedulerStore])
 
+
+    const handleOpen = () => setOpen(true);
+    
+    const handleSearch = async(filter: ISchedulerSearchParams) => {
+        setLoading(true);
+        await schedulerStore.search(filter);
+        setLoading(false);
+    };
+
+    const columns : ColumnDef<IScheduler>[] = [
+        {
+            header : '',
+            size : 60,
+            accessorKey : 'schedulerKey',
+            id : 'key'
+        },
+        {
+            header : '제목',
+            size : 100,
+            accessorKey : 'schedulerTitle'
+        },
+        {
+            header : '내용',
+            size : 100,
+            accessorKey : 'schedulerContent'
+        },
+        {
+            header : '작업 시작 시간',
+            size : 100,
+            accessorKey : 'schedulerStartTime'
+        },
+        {
+            header : '작업 반복 여부',
+            size : 100,
+            accessorKey : 'schedulerRepetition'
+        },
+        {
+            header : '작업 끝나는 시간',
+            size : 100,
+            accessorKey : 'schedulerEndTime'
+        },
+        {
+            header : '작업 일시',
+            size : 100,
+            accessorKey : 'schedulerDate'
+        }
+    ]
+    const table = useReactTable<IScheduler>({
+        data:schedulerStore.schedulerList,
+        columns,
+        columnResizeMode,
+        state:{sorting, columnVisibility},
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        initialState: {
+            pagination : {
+                pageSize: 15
+            }
+        }
+    });
 
 
 
     return (
         <div>
             <h1>test중입니다</h1>        
+            <Table table={table} resizeMode={columnResizeMode} loadingStatus={loading}/>
         </div>
+        
     )
 }
 
